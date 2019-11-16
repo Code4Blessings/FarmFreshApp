2e8b87838f35efce060330e0d92f0cdbd1f372cb
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-11-16 13:32:55 -0500

    Redux installs and comps set up

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export const LOGIN_ATTEMPT = 'LOGIN_ATTEMPT';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOG_OUT = 'LOG_OUT';
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const DELETE_CART_ITEM = 'DELETE_CART_ITEM';
+export const UPDATE_CART_ITEM = 'UPDATE_CART_ITEM';
