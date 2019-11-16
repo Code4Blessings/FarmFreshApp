0de9b917c479b96176a8b0a6bcfb96e23b38699d
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-11-16 17:35:23 -0500

    actions setup and dummy data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
 export const LOGIN_ATTEMPT = 'LOGIN_ATTEMPT';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
@@ -5,3 +7,25 @@ export const LOG_OUT = 'LOG_OUT';
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const DELETE_CART_ITEM = 'DELETE_CART_ITEM';
 export const UPDATE_CART_ITEM = 'UPDATE_CART_ITEM';
+
+export const fetchUser = () => (dispatch) => {
+	dispatch({ type: LOGIN_ATTEMPT });
+	axiosWithAuth
+		.length('')
+		.then((res) => {
+			console.log(res.data);
+			return res;
+		})
+		.then((res) =>
+			dispatch({
+				type    : LOGIN_SUCCESS,
+				payload : res.data,
+			}),
+		)
+		.catch((err) =>
+			dispatch({
+				type    : LOGIN_FAIL,
+				payload : err.response,
+			}),
+		);
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducer';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById('root'),
+);
