8f3cf43ab72278e816cde418b124581ea75f5f8d
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-16 11:07:48 -0500

    header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {Route, Link} from "react-router-dom";
 import './App.css';
+import InitialSignInPage from './InitialSignInPage';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<>
+			<Route exact path='/' component={InitialSignInPage}/>
+		</>
+	);
 }
 
 export default App;
