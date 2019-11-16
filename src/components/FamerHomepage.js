2a36be48730616a8dac68d0eafc19cb591b5f28d
Code4Blessings <robinwarden@code4blessings.com>
2019-11-16 17:12:05 -0500

    Completed FarmHomepage

---
+++ src/components/FamerHomepage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
+
+const FarmerHomepage = () => {
+    return (
+        <div className="homepage-container">
+            <div className="header">
+                <Header />
+            </div>
+            <div className="homepage-links">
+                <Link>View Orders</Link>
+                 <Link>Add/Edit Inventory</Link>
+            </div>
+            <div className="homepage-links">
+                <Link>Add Upcoming Harvests</Link>
+                 <Link>View Farm Profile</Link>
+            </div>
+        </div>
+        
+    );
+}
+
+export default FarmerHomepage;
