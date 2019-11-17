2800526a32c1e75885bd8e6fc72d476364550144
Code4Blessings <robinwarden@code4blessings.com>
2019-11-17 17:15:50 -0500

    Completed top portion of AddInventory form and made a link to it from the FarmerHomepage

--- src/components/AddInventory/AddInventory.js
+++ src/components/AddInventory/AddInventory.js
@@ -2,6 +2,7 @@ import React from "react";
 import { formik } from "formik";
 import Header from "../Header";
 import AddButton from "./images/add-button.png";
+import "./AddInventory.css";
 
 const AddInventory = () => {
    return (
@@ -12,21 +13,15 @@ const AddInventory = () => {
         <div className="add-inventory-title">
             <h1>Add/Inventory</h1>
         </div>
-        <div className="input-container">
-            <div className="quantity-input">
-                <input 
-                type="text"
-                placeholder="1" />
-            </div>
-            <div className="inventory-input">
-                <input 
+        <form className="inventory-form">
+            <input className="quantity-inventory"
+            type="text"
+            placeholder="1" />
+                <input className="inventory-input"
                 type="text"
                 placeholder="Item Name" />
-            </div>
-            <div className="add-btn">
-                <img src={AddButton} alt="plus button"/>
-            </div>
-        </div>
+            <button className="add-btn">+</button>
+        </form>
       </div>
 
    );
