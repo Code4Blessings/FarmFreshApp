bf7152c72b4660fbeba319ae002523f68ecd711c
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-19 13:21:22 -0500

    finished shopper dashboard

--- src/components/ShopperHomepage.js
+++ src/components/ShopperHomepage.js
@@ -6,8 +6,48 @@ const ShopperHomepage = () => {
 	return (
 		<div>
 			<Header/>
+			{/* log out button needed here */}
 			<div className='shopper-homepage-container'>
-				<h2>Under construction...</h2>
+				<h2>Choose Your Farm:</h2>
+				<div className='farm-container'>
+					<div className='farm-row'>
+						<div>
+							<h3>Shane Farms</h3>
+							<p>1122 Redwood Ave.</p>
+							<p>Oakland, CA 94606</p>
+						</div>
+						<div>
+							<p>Currently Available:</p>
+							<p>Pumpkins, Squash, Corn</p>
+							<a href=''>view full inventory</a>
+						</div>
+					</div>
+					<div className='farm-row'>
+						<div>
+							<h3>Shane Farms</h3>
+							<p>1122 Redwood Ave.</p>
+							<p>Oakland, CA 94606</p>
+						</div>
+						<div>
+							<p>Currently Available:</p>
+							<p>Pumpkins, Squash, Corn</p>
+							<a href=''>view full inventory</a>
+						</div>
+					</div>
+					<div className='farm-row'>
+						<div>
+							<h3>Shane Farms</h3>
+							<p>1122 Redwood Ave.</p>
+							<p>Oakland, CA 94606</p>
+						</div>
+						<div>
+							<p>Currently Available:</p>
+							<p>Pumpkins, Squash, Corn</p>
+							<a href=''>view full inventory</a>
+						</div>
+					</div>
+				</div>
+				<button className='farmer-sign-in-button button-spacing'>Next</button>
 			</div>
 		</div>
 	);
