1c4c3b2ae8998ed520188a7e74887e740b12eff4
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-19 21:18:37 -0500

    removed header import on files that did not need it

--- src/components/FarmerEditInventory.js
+++ src/components/FarmerEditInventory.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import Header from './Header';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
