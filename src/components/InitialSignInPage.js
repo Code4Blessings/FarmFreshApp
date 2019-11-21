ff0aa6cb9bf8186ac866e7b73db78c45aaec516b
Code4Blessings <robinwarden@code4blessings.com>
2019-11-21 13:11:56 -0500

    Animated initial signin page buttons using GSAP

--- src/components/InitialSignInPage.js
+++ src/components/InitialSignInPage.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import Header from './Header';
+import { gsap } from 'gsap';
 
 const InitialSignInPage = () => {
+    let slideButton1 = useRef(null);
+    let slideButton2 = useRef(null);
+    
+    useEffect(() => {
+        gsap.to(slideButton1,{ duration: 2.0, ease: "back.in(1.0)", y: -80 });
+        gsap.to(slideButton2,{ duration: 2.0, ease: "back.in(1.0)", y: 90 });
+        
+    },[])
+
     return (
         <div>
             <Header/>
             <section className='initial-sign-in-page-section'>
                 <div>
                     <h2>Welcome!</h2>
-                    <Link to='/farmer/login'><button className='farmer-sign-in-button'>Farmer Sign In</button></Link>
-                    <Link to='/shopper/login'><button className='shopper-sign-in-button'>Shopper Sign In</button></Link>
+                    <Link to='/farmer/login'><button 
+                    ref={button2 => { slideButton2 = button2 }} 
+                    className='farmer-sign-in-button'>Farmer Sign In</button></Link>
+                    <Link to='/shopper/login'><button
+                    ref={button1 => {slideButton1 = button1}} 
+                    className='shopper-sign-in-button'>Shopper Sign In</button></Link>
                     <Link to='/register'><p>Create an Account</p></Link>
                 </div>
             </section>
