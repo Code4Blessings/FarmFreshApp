aec004e368b70f4079607b946c8aabab34e64e3e
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-21 18:47:11 -0500

    added fade in animation to initial sign in page

--- src/components/InitialSignInPage.js
+++ src/components/InitialSignInPage.js
@@ -1,32 +1,32 @@
-import React, {useRef, useEffect} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import HeaderWithLinkBackToLandingPage from './HeaderWithLinkBackToLandingPage';
-import {gsap} from 'gsap';
+import {TweenMax} from 'gsap';
+
+if (document.readyState === "interactive" || document.readyState === "complete") {
+    resolve();
+} else {
+    window.addEventListener("DOMContentLoaded", resolve);
+}
+
+function resolve() {
+    TweenMax.from("#content", 1, {
+        opacity: -2,
+        y: 100
+    });
+}
 
 const InitialSignInPage = () => {
-    let slideButton1 = useRef(null);
-    let slideButton2 = useRef(null);
-    
-    useEffect(() => {
-        gsap.to(slideButton1,{ duration: 3.5, ease: "back.in(1.0)", y: 90 });
-        gsap.to(slideButton2,{ duration: 3.5, ease: "back.in(1.0)", y: -80 });
-        
-    },[])
-
     return (
         <div>
             <HeaderWithLinkBackToLandingPage/>
             <section className='initial-sign-in-page-section'>
-                <div>
+                <div id='content'>
                     <h2>Welcome!</h2>
 
-                   <Link to='/shopper/login'><button
-                    ref={button1 => {slideButton1 = button1}} 
-                    className='shopper-sign-in-button'>Shopper Sign In</button></Link>
+                   <Link to='/shopper/login'><button className='shopper-sign-in-button'>Shopper Sign In</button></Link>
 
-                    <Link to='/farmer/login'><button 
-                    ref={button2 => { slideButton2 = button2 }} 
-                    className='farmer-sign-in-button'>Farmer Sign In</button></Link>
+                    <Link to='/farmer/login'><button className='farmer-sign-in-button'>Farmer Sign In</button></Link>
 
                     <Link to='/register'><p>Create an Account</p></Link>
                 </div>
